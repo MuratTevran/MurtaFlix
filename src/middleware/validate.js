d03b267f84f78a692d0b2bb958174d75549f9a02
Murat Tevran <murattevran@gmail.com>
2022-10-13 17:40:27 +0300

    validate,id controller, admin ve normal userlar için authenticate işlemi yaptığımız js dosyası eklendi.

---
+++ src/middleware/validate.js
@@ -0,0 +1,15 @@
+const hs = require("http-status")
+
+const validate = (schema) => (req,res,next) => {
+    const { value,error } = schema.validate(req.body)
+    if(error) {
+        const errorMessage = error.details?.map((details) => details.message)
+        .join(",")
+
+        return res.status(js.BAD_REQUEST).send({message: errorMessage})
+    }
+    Object.assign(req,value)
+    next()
+}
+
+module.exports = validate
